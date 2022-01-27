a482cd0ca848da677ee2bf3b44200225a1a0012e
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2022-01-27 09:34:59 +0100

    SnackBar

---
+++ lib/pages/page_snack.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class PageSnack extends StatefulWidget {
+  const PageSnack({Key? key}) : super(key: key);
+
+  @override
+  _PageSnackState createState() => _PageSnackState();
+}
+
+class _PageSnackState extends State<PageSnack> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SNACKBAR"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: callSnack,
+              child: Text("SNACK !")
+          )
+        ],
+      ) ,
+    );
+  }
+
+  void callSnack(){
+    SnackBar snackBar = SnackBar(
+        content: Text("Enregistrement OK"),
+      duration: Duration(seconds: 3),
+      backgroundColor: Colors.black38,
+      action: SnackBarAction(
+        label: "Annuler",
+        onPressed: (){
+          print("Annuler");
+        },
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+}
